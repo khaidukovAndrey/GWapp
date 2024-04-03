dbe95ffe88df2de9eb9181d6369cc555d005206c
khaidukovAndrey <117283349+khaidukovAndrey@users.noreply.github.com>
2024-04-03 22:30:09 +0700

    Start dev image picker

--- app/src/main/java/com/example/kotlin_lesson_1/ScanMenuActivity.kt
+++ app/src/main/java/com/example/kotlin_lesson_1/ScanMenuActivity.kt
@@ -1,12 +1,22 @@
 package com.example.kotlin_lesson_1
 
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.kotlin_lesson_1.databinding.ActivityScanMenuBinding
 
 class ScanMenuActivity : AppCompatActivity() {
     private lateinit var binding : ActivityScanMenuBinding
+    var pickedPhoto : Uri? = null
+    var pickedBitMap : Bitmap? = null
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         try{
@@ -19,5 +29,43 @@ class ScanMenuActivity : AppCompatActivity() {
             val intent = Intent(this, CameraActivity::class.java)
             startActivity(intent)
         }
+        binding.scanMenuLoadPhoto.setOnClickListener {
+            val intent = Intent(this, CameraActivity::class.java)
+            pickPhoto()
+        }
+
+
+    }
+    private fun pickPhoto(){
+        if (ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) { // izin alınmadıysa
+            ActivityCompat.requestPermissions(this,arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                1)
+        } else {
+            val galeriIntext = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(galeriIntext,2)
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 1) {
+            if (grantResults.size > 0  && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                val galeriIntext = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galeriIntext,2)
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == 2 && resultCode == Activity.RESULT_OK && data != null) {
+            pickedPhoto = data.data
+            if (pickedPhoto != null) {
+                Log.d("MyLog", pickedPhoto.toString());
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
